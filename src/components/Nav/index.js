fe45e5ae51a0b039c0ae339cebf078d206486351
Graham Hall <ghall89@me.com>
2021-06-09 11:52:07 -0400

    conditionally render gallery pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-const categories = [
-	{
-		name: "commercial",
-		description:
-			"Photos of grocery stores, food trucks, and other commercial projects",
-	},
-	{ name: "portraits", description: "Portraits of people in my life" },
-	{ name: "food", description: "Delicious delicacies" },
-	{
-		name: "landscape",
-		description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-	},
-];
+function Nav(props) {
 
-function categorySelected(name) {
-	alert(`${name} clicked!`);
-}
-
-function Nav() {
+	const {
+		categories = [],
+		setCurrentCategory,
+		currentCategory
+	} = props;
 	
-	return(
-		<header>
+	useEffect(() => {
+		document.title = capitalizeFirstLetter(currentCategory.name);
+	}, [currentCategory])
+
+	return (
+		<header className="flex-row px-1">
 			<h2>
 				<a data-testid="link" href="/">
-					<span role="img" aria-label="camera"> 📸</span> Oh Snap!
+					<span role="img" aria-label="camera">
+						{" "}
+						📸
+					</span>{" "}
+					Oh Snap!
 				</a>
 			</h2>
 			<nav>
 				<ul className="flex-row">
 					<li className="mx-2">
-						<a data-testid="about" href="#about">
+						<a
+							href="#about"
+						>
 							About me
 						</a>
 					</li>
@@ -38,20 +37,22 @@ function Nav() {
 						<span>Contact</span>
 					</li>
 					{categories.map((category) => (
-						<li
-							className="mx-1"
-							key={category.name}
-						>
-							<span onClick={() => categorySelected(category.name)} >
-								{category.name}
+						<li className={`mx-1 ${
+								currentCategory.name === category.name && 'navActive'
+								}`} key={category.name}>
+							<span
+								onClick={() => {
+									setCurrentCategory(category)
+								}}
+							>
+								{capitalizeFirstLetter(category.name)}
 							</span>
 						</li>
 					))}
-				</ul>	
+				</ul>
 			</nav>
 		</header>
 	);
-	
 }
 
 export default Nav;
